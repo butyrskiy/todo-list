cf3d84b588b07e7967bec9a0bcd911e2b6f4dab0
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-07-13 19:41:50 +0400

    version 2.0.0. Transition from object to array

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const todo = {};
+const todo = [];
 
 const STATUS = {
   'TODO': 'To do',
@@ -6,27 +6,41 @@ const STATUS = {
   'DONE': 'Done'
 }
 
+const PRIORITY = {
+  'HIGH': 'High',
+  'MIDDLE': 'Middle',
+  'LOW': 'Low'
+}
+
 function addTask(name) {
   if(typeof(name) !== 'string') {
     console.error('The task name must be a string!');
   } else {
-    todo[name] = STATUS.TODO;
+    todo.push({
+      name: name,
+      status: STATUS.TODO,
+      priority: PRIORITY.MIDDLE
+    });
   }
 }
 
-function changeStatus(name, status) {
-  if(name in todo) {
-    todo[name] = status;
+function changeStatus(nameTask, status) {
+  let task = findTaskInTodo(nameTask);
+  
+  if(task) {
+    task.status = status;
   } else {
-    console.error(`This task: «${name}» is not on this list!`);
+    console.error(`This task: «${nameTask}» is not on this list!`)
   }
 }
 
-function deleteTask(name) {
-  if(name in todo) {
-    delete todo[name];
+function deleteTask(nameTask) {
+  let task = findTaskIndex(nameTask);
+
+  if(typeof(task) === 'number') {
+    todo.splice(task, 1);
   } else {
-    console.error(`This task: «${name}» is not on this list!`);
+    console.error(task);
   }
 }
 
@@ -34,42 +48,64 @@ function showList() {
   let todoStr = '';
   let inProgressStr = '';
   let doneStr = '';
-
-  for(key in todo) {
-    if(todo[key] === STATUS.TODO) {
-      todoStr += `${key}: ${todo[key]}\n`;
-    } else if(todo[key] === STATUS.IN_PROGRESS) {
-      inProgressStr += `${key}: ${todo[key]}\n`;
-    } else if(todo[key] === STATUS.DONE) {
-      doneStr += `${key}: ${todo[key]}\n`;
+  
+  for(let task of todo) {
+    if(task.status === STATUS.TODO) {
+      todoStr += `${task.name}\n\t`;
+    } else if(task.status === STATUS.IN_PROGRESS) {
+      inProgressStr += `${task.name}\n\t`;
+    } else if(task.status === STATUS.DONE) {
+      doneStr += `${task.name}\n\t`;
     }
   }
-  
-  console.log(`${todoStr}\n${inProgressStr}\n${doneStr}`);
-  
-  if(todoStr === '') {
-    todoStr += `\nNothing is «${STATUS.TODO}»`;
-    console.log(todoStr);
-  } 
-  
-  if(inProgressStr === '') {
-    inProgressStr += `\nNothing is «${STATUS.IN_PROGRESS}»`;
-    console.log(inProgressStr);
-  } 
-  
-  if(doneStr === '') {
-    doneStr += `\nNothing is «${STATUS.DONE}»`;
-    console.log(doneStr);
+
+    if(todoStr !== '') {
+      console.log(`To do:\n\t${todoStr}\n`);
+    } else {
+      todoStr += `\nNothing is «${STATUS.TODO}»`;
+      console.log(todoStr);
+    }
+
+    if(inProgressStr !== '') {
+      console.log(`In progress:\n\t${inProgressStr}`);
+    } else {
+      inProgressStr += `\nNothing is «${STATUS.IN_PROGRESS}»`;
+      console.log(inProgressStr);
+    }
+
+    if(doneStr !== '') {
+      console.log(`Done:\n\t${doneStr}`);
+    } else {
+      doneStr += `\nNothing is «${STATUS.DONE}»`;
+      console.log(doneStr);
+    }
+}
+
+function findTaskInTodo(nameTask) {
+  let task = todo.find(item => item.name === nameTask);
+
+  if(task) {
+    return task;
+  }
+}
+
+function findTaskIndex(nameTask) {
+  let task = todo.findIndex(item => item.name === nameTask);
+
+  if(task >= 0) {
+    return task;
+  } else {
+    return `This task: «${nameTask}» is not on this list!`
   }
 }
 
 addTask('walk the dog');
 addTask('create a task list');
 addTask('read new article');
-addTask('buy milk')
+addTask('buy milk');
 
 changeStatus('walk the dog', STATUS.TODO);
-changeStatus('create a task list', STATUS.TODO);
+changeStatus('create a task list', STATUS.IN_PROGRESS);
 changeStatus('read new article', STATUS.DONE);
 
 deleteTask('buy milk');
